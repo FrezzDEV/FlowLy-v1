9e1de9de56a35f0477c80050d5e1cc4d996cf835
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-08-26 15:53:09 +0300

    feat: add trending tracks to music repository contract

--- lib/domain/repositories/music_repository.dart
+++ lib/domain/repositories/music_repository.dart
@@ -2,5 +2,6 @@ import '../../data/models/track_model.dart';
 
 abstract interface class MusicRepository {
   Future<List<FlowLyTrack>> search(String query);
+  Future<List<FlowLyTrack>> trending();
   String streamUrl(String videoId);
 }
