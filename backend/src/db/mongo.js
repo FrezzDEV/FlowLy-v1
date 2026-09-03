7dfd39c7564dc03adc982c2cf731ad1a34420860
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-09-03 15:53:57 +0300

    Delete backend

--- backend/src/db/mongo.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectMongo(uri) {
-  if (!uri) return false;
-  await mongoose.connect(uri, { serverSelectionTimeoutMS: 5000 });
-  return true;
-}
